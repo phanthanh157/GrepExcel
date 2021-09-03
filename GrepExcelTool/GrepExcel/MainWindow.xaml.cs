3e83560c9a464b38b576490f236fdce8faf5e4c2
phan thanh <phanthanh157@gmail.com>
2021-09-03 22:09:52 +0700

    add key shortcut open setting search and about

--- GrepExcelTool/GrepExcel/MainWindow.xaml.cs
+++ GrepExcelTool/GrepExcel/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GrepExcel
         {
             InitializeComponent();
         }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
     }
 }
