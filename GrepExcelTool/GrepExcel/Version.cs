837308d8c4b65d0aeaa4fd98d198ee052900be90
phan thanh <phanthanh157@gmail.com>
2022-08-21 09:43:52 +0700

    Add version Major = 1 Minor = 0 Build = 0 Phien ban nay su dung search cu cach serach cu. Sau phien ban nay se thay doi toan bo cau hinh

---
+++ GrepExcelTool/GrepExcel/Version.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrepExcel
+{
+    public class Version
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+
+        public Version()
+        {
+            Major = 1;
+            Minor = 0;
+            Build = 0;
+        }
+    }
+}
